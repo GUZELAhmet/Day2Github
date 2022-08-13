350342ce38dc690268447a12d5287e138b2d0075
Ahmet Guzel <gzlhmt@gmail.com>
2022-08-13 22:36:49 +0300

    4.C

--- src/day1Git.java
+++ src/day1Git.java
@@ -4,5 +4,6 @@ public class day1Git {
         System.out.println("Hello  git 1. Deneme");
         System.out.println("Hello  git 2. Deneme");
         System.out.println("Hello  git 3. Deneme");
+        System.out.println("Hello  git 4. Deneme");
     }
 }
